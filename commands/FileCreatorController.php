8cdc71b93427563d8ed51651ef3d8e1c4d6e2d8a
whiteromka <whiteromka@yandex.ru>
2022-03-16 20:52:48 +0300

    empty yii2 project

---
+++ commands/FileCreatorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+class FileCreatorController extends Controller
+{
+    public function actionCreate()
+    {
+
+    }
+}
